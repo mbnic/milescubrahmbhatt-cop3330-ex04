1527e5d69501aaba37ca15b60a6bce829b9ac465
nic <mbnic@icloud.com>
2021-06-05 23:28:29 -0400

    final base commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,48 @@
+package base;
+
+import java.util.Scanner;
+
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App myApp = new App();
+
+        String noun = myApp.getNoun();
+        String verb = myApp.getVerb();
+        String adjective = myApp.getAdjective();
+        String adverb = myApp.getAdverb();
+
+        myApp.printMadlib(noun, verb, adjective, adverb);
+    }
+
+
+    public void printMadlib(String noun, String verb, String adj, String adv) {
+        System.out.printf("Do you %s your %s %s %s?", verb, adj, noun, adv);
+    }
+
+
+    public String getNoun() {
+        System.out.println("Enter a noun: ");
+        String word = in.nextLine();
+        return word;
+    }
+
+    public String getVerb() {
+        System.out.println("Enter a verb: ");
+        String word = in.nextLine();
+        return word;
+    }
+
+    public String getAdjective() {
+        System.out.println("Enter an adjective: ");
+        String word = in.nextLine();
+        return word;
+    }
+
+    public String getAdverb() {
+        System.out.println("Enter an adverb: ");
+        String word = in.nextLine();
+        return word;
+    }
+}
